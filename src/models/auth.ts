a094a6fd27fb83c72a7205e998f1a5efde234172
Jesus Moreira <jesucristhorr@protonmail.com>
2023-01-14 23:54:52 -0500

    [Auth] Change signup schema

--- src/models/auth.ts
+++ src/models/auth.ts
@@ -2,7 +2,7 @@ import z from 'zod';
 
 export const postLoginModel = z.object({
     email: z.string().email(),
-    password: z.string().min(6).max(32),
+    password: z.string().min(10).max(32),
 });
 
 export type PostLogin = z.infer<typeof postLoginModel>;
@@ -11,7 +11,7 @@ export const postSignupModel = z.object({
     displayName: z.string().min(2).max(255),
     email: z.string().email().max(255),
     username: z.string().min(4).max(24),
-    password: z.string().min(6).max(32),
+    password: z.string().min(10).max(32),
     roleId: z.number().int(),
 });
 
