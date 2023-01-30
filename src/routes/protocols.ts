fb69c0bd2fa970f35d3f282f4b17032bc45000c7
Jesus Moreira <jesucristhorr@protonmail.com>
2023-01-30 00:00:17 -0500

    [Sensors] Add GET protocols

---
+++ src/routes/protocols.ts
@@ -0,0 +1,34 @@
+import { FastifyPluginAsync } from 'fastify';
+
+const routes: FastifyPluginAsync = async (fastify) => {
+    fastify.get(
+        '/',
+        { onRequest: [fastify.jwtAuthentication] },
+        async ({ user }, reply) => {
+            if (!['System', 'Admin'].includes(user.role.name))
+                return reply
+                    .status(403)
+                    .send({ code: 403, msg: `You don't have access to this resource` });
+
+            const protocols = await fastify.prisma.connectionType.findMany({
+                select: {
+                    id: true,
+                    protocol: true,
+                    createdAt: true,
+                    updatedAt: true,
+                },
+                where: {
+                    deletedAt: null,
+                },
+            });
+
+            return {
+                protocols,
+            };
+        }
+    );
+};
+
+export const autoPrefix = '/protocols';
+
+export default routes;
