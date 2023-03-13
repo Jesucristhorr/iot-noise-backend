20e4587f949900e258b18d46b2e3ad24bb6b3d05
Jesus Moreira <jesucristhorr@protonmail.com>
2023-03-13 06:30:38 -0500

    [Chore] Add connection details to prisma schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -59,6 +59,115 @@ if (!process.env.DEFAULT_SYSTEM_USER_PASSWORD)
     });
 
     console.log('Done!');
+    console.log('Creating default connection details...');
+
+    await prisma.connectionDetail.create({
+        data: {
+            connectionType: {
+                connect: {
+                    protocol: 'MQTT',
+                },
+            },
+            details: {
+                formTitle: 'Parámetros de conexión para MQTT',
+                fields: [
+                    {
+                        fieldId: 'connectionUrl',
+                        fieldLabel: 'Url de conexión',
+                        fieldMandatory: true,
+                        fieldPlaceholder: 'mqtt://test.mosquitto.org',
+                        fieldType: 'text',
+                        fieldDefaultValue: '',
+                        fieldValidationType: 'url',
+                        fieldValidationFunction: null,
+                        fieldValidationErrorText:
+                            'Por favor ingrese una url de conexión válida',
+                    },
+                    {
+                        fieldId: 'username',
+                        fieldLabel: 'Usuario MQTT',
+                        fieldMandatory: true,
+                        fieldPlaceholder: 'admin',
+                        fieldType: 'text',
+                        fieldDefaultValue: '',
+                        fieldValidationType: 'regex',
+                        fieldValidationFunction: {
+                            arguments: 'value',
+                            body: 'return /[\\w\\W]{2,}/gi.test(value)',
+                        },
+                        fieldValidationErrorText:
+                            'Por favor ingrese un usuario mínimo de 2 caracteres',
+                    },
+                    {
+                        fieldId: 'password',
+                        fieldLabel: 'Contraseña MQTT',
+                        fieldMandatory: false,
+                        fieldPlaceholder: 'contraseña',
+                        fieldType: 'password',
+                        fieldDefaultValue: '',
+                        fieldValidationType: 'none',
+                        fieldValidationFunction: null,
+                        fieldValidationErrorText: '',
+                    },
+                ],
+            },
+        },
+    });
+
+    await prisma.connectionDetail.create({
+        data: {
+            connectionType: {
+                connect: {
+                    protocol: 'MQTTS',
+                },
+            },
+            details: {
+                formTitle: 'Parámetros de conexión para MQTTS',
+                fields: [
+                    {
+                        fieldId: 'connectionUrl',
+                        fieldLabel: 'Url de conexión',
+                        fieldMandatory: true,
+                        fieldPlaceholder: 'mqtt+ssl://test.mosquito.org',
+                        fieldType: 'text',
+                        fieldDefaultValue: '',
+                        fieldValidationType: 'url',
+                        fieldValidationFunction: null,
+                        fieldValidationErrorText:
+                            'Por favor ingrese una url de conexión válida',
+                    },
+                    {
+                        fieldId: 'username',
+                        fieldLabel: 'Usuario MQTT',
+                        fieldMandatory: true,
+                        fieldPlaceholder: 'admin',
+                        fieldType: 'text',
+                        fieldDefaultValue: '',
+                        fieldValidationType: 'regex',
+                        fieldValidationFunction: {
+                            arguments: 'value',
+                            body: 'return /[\\w\\W]{2,}/gi.test(value)',
+                        },
+                        fieldValidationErrorText:
+                            'Por favor ingrese un usuario mínimo de 2 caracteres',
+                    },
+                    {
+                        fieldId: 'password',
+                        fieldLabel: 'Contraseña MQTT',
+                        fieldMandatory: false,
+                        fieldPlaceholder: 'contraseña',
+                        fieldType: 'password',
+                        fieldDefaultValue: '',
+                        fieldValidationType: 'none',
+                        fieldValidationFunction: null,
+                        fieldValidationErrorText: '',
+                    },
+                ],
+            },
+        },
+    });
+
+    console.log('Done!');
 })()
     .then(async () => {
         await prisma
